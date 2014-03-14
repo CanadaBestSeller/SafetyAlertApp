7df8d249f162dc05a2171943521518c93dedf8c4
SourceDave <david.d.li@gmail.com>
2014-03-14 00:35:51 -0700

    Added dialog accept option.

--- SafetyAlert/src/com/example/safetyalert/DialogManager.java
+++ SafetyAlert/src/com/example/safetyalert/DialogManager.java
@@ -3,6 +3,7 @@ package com.example.safetyalert;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.WindowManager;
 
 public class DialogManager {
@@ -22,12 +23,22 @@ public class DialogManager {
 		builder.setMessage("Your friend would like you to be their guardian for "
 				+ Integer.toString(g.guardianshipDuration) + " minutes.\n\n^ Check your notifications!");
 
-		builder.setPositiveButton("Okay",
+		builder.setPositiveButton("Accept",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						Intent toGuardianModeActivity = new Intent(context, GuardianModeActivity.class);
+						toGuardianModeActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						toGuardianModeActivity.putExtra(GuardianModeAlarm.EXTRA_GUARDIAN_REQUEST, g);
+				        context.startActivity(toGuardianModeActivity);
+					}
+		});
+
+		builder.setNegativeButton("Later",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
 						dialog.dismiss();
 					}
-		});
+		});	
 
 		AlertDialog alert = builder.create();
 		alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
@@ -43,10 +54,20 @@ public class DialogManager {
 		builder.setPositiveButton("Respond",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
-						dialog.dismiss();
-					}
+				        Intent toAlertResponseActivity = new Intent(context, AlertResponseActivity.class);
+				        toAlertResponseActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				        toAlertResponseActivity.putExtra(GuardianModeAlarm.EXTRA_GUARDIAN_REQUEST, g);
+				        context.startActivity(toAlertResponseActivity);
+				    }
 		});
 		
+		builder.setNegativeButton("Later",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						dialog.dismiss();
+					}
+		});	
+
 		AlertDialog alert = builder.create();
 		alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
 		alert.show();
--- SafetyAlert/src/com/example/safetyalert/GuardianModeActivity.java
+++ SafetyAlert/src/com/example/safetyalert/GuardianModeActivity.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class GuardianModeActivity extends Activity {
 
@@ -24,13 +26,15 @@ public class GuardianModeActivity extends Activity {
 		g = (GuardianRequest) data
 				.getParcelable(GuardianModeAlarm.EXTRA_GUARDIAN_REQUEST);
 		sorry = i.getBooleanExtra(TimeoutQuestionnaire.SORRY, false);
-		
+
 		// normal case
 		if (g.interval != 0) {
-			past = (System.currentTimeMillis() > (g.triggerTime + 15*60*1000))? true : false;
+			past = (System.currentTimeMillis() > (g.triggerTime + 15 * 60 * 1000)) ? true
+					: false;
 		} else {
 			// test case
-			past = (System.currentTimeMillis() > (g.triggerTime + 10000))? true : false;
+			past = (System.currentTimeMillis() > (g.triggerTime + 10000)) ? true
+					: false;
 		}
 
 		// If late acceptance, should only happen once
@@ -50,12 +54,24 @@ public class GuardianModeActivity extends Activity {
 			nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 			nManager.cancel(GuardianModeActivity.GUARDIAN_MODE_NOTIFICATION_ID);
 
-			alertAlarm = new AlertAlarm();
-			alertAlarm.setAlert(this, g);
 		}
 	}
 
 	public void quit(View view) {
+
+		EditText et = (EditText) findViewById(R.id.guardian_mode_feedback);
+		String feedback = et.getText().toString();
+
+		Utils.appendToLog("[GUARDIAN MODE FEEDBACK] " + feedback);
+
+		alertAlarm = new AlertAlarm();
+		alertAlarm.setAlert(this, g);
+
+		Toast.makeText( this,
+				"Stay tuned. For the next " + g.guardianshipDuration
+						+ " minutes, you might receive an alert!",
+				Toast.LENGTH_LONG).show();
+
 		this.finish();
 	}
 }
